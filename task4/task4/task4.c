cb51f2681a85be2b4449a997c229d145845feae0
vladislav-miroshnikov <vladislaw.miroshnikov@gmail.com>
2019-10-07 01:14:33 +0300

    task4.c

---
+++ task4/task4/task4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+int simple(long long n)
+{
+	for (long long i = 2; i * i <= n; i++)
+		if ((n % i) == 0)
+			return 0;
+	return 1;
+}
+int main()
+{
+	double c = 2;
+	for (int j = 2; j <= 31; j++)
+	{
+		if (simple(j))
+		{
+			long long a = pow(c, j) - 1;
+			if (simple(a))
+				printf("%lld\n", a);
+		}
+	}
+	return 0;
+}
