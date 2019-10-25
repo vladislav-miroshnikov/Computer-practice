167ac8750d1448b506d7ef9541e790f09447e6fe
vladislav-miroshnikov <vladislaw.miroshnikov@gmail.com>
2019-10-25 13:04:17 +0300

    task1 v.2
    added function using dynamic array

--- task1/task1/task1.c
+++ task1/task1/task1.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <malloc.h>
 #include <math.h>
 #include <string.h>
+
+void loc(long long r,int n)
+{
+	int *a, i = 0;
+	a = (int*)malloc(n * sizeof(int));
+	
+	while (r != 0)
+	{			
+		a[i] = r % 2;
+		r = r / 2;
+		i++;
+	}
+	
+	for (i = 0; i <= (n-1); i++)
+		printf("%d", a[(n-1)-i]);
+	free(a);
+}
 
 int main()
 {
@@ -8,57 +26,20 @@ int main()
 	const char surname[] = "Miroshnikov";
 	const char patronymic[] = "Igorevich";
 	int proizv = strlen(name) * strlen(surname) * strlen(patronymic);
-	printf("Composition is equal: %d\n", proizv);
+	printf("The product is equal: %d\n", proizv);
 	long long b = pow(2, 32) - proizv;
-	int t[32], i = 0;
-	
-	while (b != 0)
-	{
-		t[i] = b % 2;
-		b = b / 2;
-		i++;
-	}
-	printf("negative 32bit integer is =");
-	for (int i = 0; i<=31 ; i++)
-	{
-		printf("%d", t[31-i]);
+	printf("negative 32bit integer is = ");
+	loc(b, 32);
 
-	}
-	
 	float f = proizv;
 	int x = *((int*)& f);
-	
-	i = 0;
-	
-	while (x)
-	{
-		t[i] = x % 2;
-		x= x / 2;
-		i++;
-	}
-	printf("\npositive floating point number (single precision, IEEE 754) is = 0");
-	
-	for (i = 1; i <= 31; i++)
-	{
-		printf("%d",t[31-i]);
-	}
-	
-	double g = 891;
+	printf("\npositive floating point number (single precision, IEEE 754) is = 0"); //0 because positive
+	loc(x, 31);
+
+	double g = proizv;
 	long long l = *((long long*)& g);
-	i = 0;
-	int z[63];
-	while (l)
-	{
-		z[i] = l % 2;
-		l = l / 2;
-		i++;
-	}
-	printf("\nneg. fl. point number (double prec., IEEE 754) is  = 1");
-	
-	for (i = 1; i <= 63; i++)
-	{
-		printf("%d", z[63 - i]);
-	}
-	
+	printf("\nneg. fl. point number (double prec., IEEE 754) is  = 1"); //1 because negative
+	loc(l, 63);
+
 	return 0;
 }
