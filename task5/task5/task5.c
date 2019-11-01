c32366928e3954967e2345eb15d45e6ace09b428
vladislav-miroshnikov <vladislaw.miroshnikov@gmail.com>
2019-11-01 18:06:28 +0300

    изменен алгоритм расчета цепной дроби

--- task5/task5/task5.c
+++ task5/task5/task5.c
@@ -6,7 +6,7 @@ double checkinput(double w)
 	char e;
 	while (1)
 	{
-		if (!scanf_s("%lf", &w) || sqrt(w) == (int)(sqrt(w)) || (w <= 1) || (w != (int)w) || getchar() != '\n')
+		if (!scanf_s("%lf", &w) || sqrt(w) == (int)(sqrt(w)) || (w != (int)w) || getchar() != '\n')
 		{
 			while ((e = getchar()) != '\n' && e != EOF);
 			printf_s("Entered incorrectly\nTry again: \n");
@@ -20,17 +20,19 @@ double checkinput(double w)
 
 void prog(double w)
 {
-	int a0 = (int)sqrt(w);
-	int i = 0, a1;
-	double x0 = sqrt(w) - a0;
+	int a0 = (int)sqrt(w), i = 0, a1, num = 1, den = 0;
 	printf("[%d; {", a0);
 
 	do
 	{
-		a1 = (int)(1 / x0);
-		x0 = (1 / x0) - a1;
-		printf(" %d,", a1);
+		a1 = a0 - den;
+		den = a1 + a0;
+		num = (w - a1 * a1) / num;
+		a1 = (int)(den / num);
+		den = den % num;
 		i++;
+		printf(" %d,", a1);
+
 	} while (a1 != (2 * a0));
 
 	printf("} ]\nperiod is = %d ", i);
