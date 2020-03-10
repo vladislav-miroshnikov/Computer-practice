c7285bf357aa38d3d97723745efb63c031a90f2b
vladislav-miroshnikov <vladislaw.miroshnikov@gmail.com>
2020-03-10 18:49:22 +0300

    внесены изменения

---
+++ semester 2/Blackjack/Blackjack/BotFisrt.cs
@@ -0,0 +1,106 @@
+﻿using System.Diagnostics;
+
+namespace Blackjack
+{
+    public class BotFisrt : AbstractMan
+    {
+
+        public BotFisrt(int _playerWallet, int _gamesCount)
+        {
+            playerWallet = _playerWallet;
+            gamesCount = _gamesCount;
+        }
+
+        public int gamesCount { get; set; }
+
+        public void MakeBet()
+        {
+            for (; ;)
+            {
+                bet = rand.Next(1, 50);
+                if (bet <= playerWallet)   //проверка, если вдруг сгенерируется ставка, превосходящая имеющиеся деньги на руках
+                {
+                    playerWallet -= bet;
+                    break;
+                }
+                
+            }
+            
+        }
+
+        private void Surrender(int bet)
+        {
+            playerWallet += (int)(bet / 2);
+            list.RemoveRange(0, list.Count);
+        }
+        
+        private void Double(ref int bet)
+        {           
+            //использую ref, чтобы ставка перезаписалась
+            playerWallet -= bet;            
+            bet *= 2;           
+        }
+
+        public void Strategy(int dealerValue, ref int bet)
+        {
+            int sum = Sum();
+            for (; ; )  //стратегия завершается, когда достигли break
+            {
+                //describe the option "STAND"
+                if (((dealerValue >= 2 && dealerValue <= 11) && (sum >= 18 && sum <= 21))
+                    || ((dealerValue >= 2 && dealerValue <= 10) && (sum == 17))
+                    || ((dealerValue >= 2 && dealerValue <= 6) && (sum >= 13 && sum <= 16))
+                    || ((dealerValue >= 4 && dealerValue <= 6) && (sum == 12)))
+                {
+                    break;                 
+                }
+                // describe the option "SURRENDER"
+                else if ((dealerValue == 11 && sum == 17)
+                    || ((dealerValue >= 10 && dealerValue <= 11) && (sum >= 15 && sum <= 16)))
+                {
+                    Surrender(bet);
+                    break;                   
+                }
+                //describe the option "HIT"
+                else if (((dealerValue >= 7 && dealerValue <= 9) && (sum >= 15 && sum <= 16))
+                    || ((dealerValue >= 7 && dealerValue <= 11) && (sum >= 12 && sum <= 14))
+                    || ((dealerValue >= 2 && dealerValue <= 3) && (sum == 12))
+                    || ((dealerValue >= 10 && dealerValue <= 11) && (sum == 10))
+                    || ((dealerValue >= 7 && dealerValue <= 11) && (sum == 9))
+                    || ((dealerValue == 2) && (sum == 9))
+                    || ((dealerValue >= 2 && dealerValue <= 11) && (sum >= 4 && sum <= 8)))
+                {
+                    GetOneCard();                   
+                }
+                //describe the option "DOUBLE"
+                else if (((dealerValue >= 2 && dealerValue <= 11) && (sum == 11))
+                    || ((dealerValue >= 2 && dealerValue <= 9) && (sum == 10))
+                    || ((dealerValue >= 3 && dealerValue <= 6) && (sum == 9))) 
+                {
+                    Double(ref bet);
+                    GetOneCard();
+                    break;                   
+                }
+
+                sum = Sum();
+                if (sum > 21)
+                {
+                    break;                  
+                }
+            }
+           
+        }
+
+        public void Info()
+        {
+            if (playerWallet <= 0) 
+            {
+                Debug.WriteLine($"bot1 lost on the {gamesCount} game");
+            }
+            else
+            {
+                Debug.WriteLine($"bot1 bank is {playerWallet}");
+            }
+        }
+    }
+}
---
+++ semester 2/Blackjack/Blackjack/Deck.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Blackjack
+{
+    public class Deck
+    {
+        public static List<Cards> cards { get; set; }
+        private Random rand = new Random();
+
+        public void CreateCards()
+        {
+            cards = new List<Cards>();         
+            for (int j = 0; j < 8; j++) 
+            {
+
+                foreach (int s in Cards.values)
+                {
+                    cards.Add(new Cards(Cards.Suits.Hearts, s));               
+                }
+
+                foreach (int s in Cards.values)
+                {
+                    cards.Add(new Cards(Cards.Suits.Diamonds, s));
+                    
+
+                }
+
+                foreach (int s in Cards.values)
+                {
+                    cards.Add(new Cards(Cards.Suits.Spades, s));
+                   
+
+                }
+
+                foreach (int s in Cards.values)
+                {
+                    cards.Add(new Cards(Cards.Suits.Clubs, s));
+                   
+                }
+                
+            }
+
+            for (int o = cards.Count - 1; o >= 1; o--)
+            {
+                int j = rand.Next(o + 1);
+                Cards tmp = cards[j];
+                cards[j] = cards[o];
+                cards[o] = tmp;
+            }
+
+        }
+
+        //public void Bring()
+        //{
+        //    for (int i = 0; i < 416; i++)
+        //    {
+        //        Console.WriteLine(cards[i]);
+        //    }
+        //    Console.WriteLine("all");
+        //}
+       
+        public void Game<T,U>(T botFirst, U botSecond) 
+            where T: BotFisrt
+            where U: BotSecond
+        {
+            Dealer dealer = new Dealer();
+            for (int a = 0; a < 400; a++) 
+            {
+                if (cards.Count <= 52) //считаем, что когда останется 52 карты и менее-перемешиваем
+                {
+                    CreateCards();
+                }
+
+                if ((botFirst.playerWallet > 0) && (botSecond.playerWallet > 0)) //случай, когда оба бота в игре
+                {
+                    botFirst.gamesCount++;
+                    botSecond.gamesCount++;                  
+                    botFirst.MakeBet();                   
+                    botSecond.MakeBet();                   
+                    botFirst.GetTwoCard();
+                    botSecond.GetTwoCard();                   
+                    dealer.GetTwoCard();
+                    if (dealer.BlackjackCheck(botFirst) == false)  //если вернется true,значит метод BlackjackCheck выполнился
+                    {
+                        botFirst.Strategy(dealer.list[0].cardValue, ref botFirst.bet);
+                        dealer.Strategy();
+                        dealer.WinnerCheck(botFirst);
+                    }
+                    if (dealer.BlackjackCheck(botSecond) == false)
+                    {
+                        botSecond.Strategy(dealer.list[0].cardValue, ref botSecond.bet);
+                        dealer.Strategy();                                                                     
+                        dealer.WinnerCheck(botSecond);
+                    }
+                    botFirst.list.RemoveRange(0, botFirst.list.Count);
+                    botSecond.list.RemoveRange(0, botSecond.list.Count);
+                    dealer.list.RemoveRange(0, dealer.list.Count);
+
+                }
+                else if ((botFirst.playerWallet > 0) && (botSecond.playerWallet <= 0)) //случай, когда только первый бот в игре
+                {
+                    botFirst.gamesCount++;
+                    botFirst.MakeBet();
+                    botFirst.GetTwoCard();                   
+                    dealer.GetTwoCard();
+                    if (dealer.BlackjackCheck(botFirst) == false)
+                    {
+                        botFirst.Strategy(dealer.list[0].cardValue, ref botFirst.bet);
+                        dealer.Strategy();
+                        dealer.WinnerCheck(botFirst);
+                    }
+                    botFirst.list.RemoveRange(0, botFirst.list.Count);                  
+                    dealer.list.RemoveRange(0, dealer.list.Count);
+                }
+                else if ((botFirst.playerWallet <= 0) && (botSecond.playerWallet > 0)) //второй бот в игре
+                {
+                    botSecond.gamesCount++;
+                    botSecond.MakeBet();
+                    botSecond.GetTwoCard();
+                    dealer.GetTwoCard();
+                    if (dealer.BlackjackCheck(botSecond) == false)
+                    {
+                        botSecond.Strategy(dealer.list[0].cardValue, ref botSecond.bet);
+                        dealer.Strategy();
+                        dealer.WinnerCheck(botSecond);
+                    }
+                    botSecond.list.RemoveRange(0, botSecond.list.Count);
+                    dealer.list.RemoveRange(0, dealer.list.Count);
+                }
+                else //оба выбыли
+                {                   
+                    break;
+                }             
+            }
+           
+        }
+    }
+}
