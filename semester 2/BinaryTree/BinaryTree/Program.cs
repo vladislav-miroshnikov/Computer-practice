6ae47c755ff29d79750743718a18cc048abec311
vladislav-miroshnikov <vladislaw.miroshnikov@gmail.com>
2020-04-16 12:17:48 +0300

    Edit CodeStyle

---
+++ semester 2/BinaryTree/BinaryTree/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BinaryTree<int> binaryTree = new BinaryTree<int>();
+
+            //binaryTree.Add(2, 4);
+            //binaryTree.Add(4, 6);
+            //binaryTree.Add(1, 5);
+            //binaryTree.Add(22, 8);
+
+            //binaryTree.GetValue(2);
+            ////binaryTree.Add(8, 3);
+            ////Console.WriteLine(binaryTree.GetValue(8));
+            ////binaryTree.RemoveByKey(8);          
+            //binaryTree.RemoveByKey(2);
+            //binaryTree.GetValue(4);
+            //Console.WriteLine(binaryTree.GetValue(1));
+
+            //Console.ReadKey();
+        }
+    }
+}
