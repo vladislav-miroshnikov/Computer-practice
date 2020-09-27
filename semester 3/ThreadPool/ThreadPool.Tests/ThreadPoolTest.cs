045315228d552a8bf09e902ad8262cf2e9f3c0bc
vladislav-miroshnikov <vladislaw.miroshnikov@gmail.com>
2020-09-27 18:38:42 +0300

    Task ThreadPool added

---
+++ semester 3/ThreadPool/ThreadPool.Tests/ThreadPoolTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ThreadPool.Tests
+{
+    [TestClass]
+    public class ThreadPoolTest
+    {
+        [TestMethod]
+        public void ThreadPoolNumberTest()
+        {
+            int[] vs = new int[50];
+            ThreadPoolLib.ThreadPool threadPool = new ThreadPoolLib.ThreadPool(34);
+            for (int i = 0; i < 50; i++)
+            {
+                threadPool.Enqueue(() => vs[i] = i >> 4);
+                Thread.Sleep(10);
+            }
+            Assert.AreEqual(34, threadPool.threads.Count);
+            for (int i = 0; i < 50; i++)
+            {
+                Assert.AreEqual(i >> 4, vs[i]);
+            }
+            Thread.Sleep(10);
+            threadPool.Dispose();
+            Assert.AreEqual(0, threadPool.threads.Count);
+        }
+    }
+}
---
+++ semester 3/ThreadPool/ThreadPoolLib/ThreadPool.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ThreadPoolLib
+{
+    public class ThreadPool : IDisposable
+    {
+        private readonly int numberOfThreads;
+        public List<Thread> threads { get; private set; }
+        private Queue<Action> actions = new Queue<Action>();
+        private volatile bool isExit = false;
+
+        public ThreadPool(int number)
+        {
+            numberOfThreads = number;
+            threads = new List<Thread>();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            for(int i = 0; i < numberOfThreads; i++)
+            {
+                threads.Add(new Thread(new ThreadStart(Exec)));
+                threads[i].Name = $"{i}";
+                threads[i].IsBackground = true;
+                threads[i].Start();
+            }
+        }
+
+        public void Enqueue(Action a)
+        {
+            lock(actions)
+            {
+                actions.Enqueue(a);
+                Monitor.PulseAll(actions);
+            }
+
+        }
+
+        
+        private void Exec()
+        {
+            while(!isExit)
+            {
+                Monitor.Enter(actions);
+                if (actions.Count != 0)
+                {
+                    Action action = actions.Dequeue();
+                    Monitor.Exit(actions);
+                    Console.Write($"\nThread {Thread.CurrentThread.Name} done task : ");
+                    action?.Invoke();
+                    Thread.Sleep(10);          
+                }
+                else
+                {
+                    Monitor.Exit(actions);                    
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            isExit = true;
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
+            threads.Clear();
+            actions = null;
+        }
+    }
+}
