6a54b3c5a36b7213847f3d24ac88ca3698a6fff1
vladislav-miroshnikov <vladislaw.miroshnikov@gmail.com>
2020-10-05 11:56:36 +0300

    1. Changed thread synchronization tool. 2. Improved IDisposable pattern

--- C# MultiThread/ThreadPool/ThreadPool/Program.cs
+++ C# MultiThread/ThreadPool/ThreadPool/Program.cs
@@ -10,7 +10,7 @@ namespace ThreadPool
             Action action = PrintHello;
             Console.WriteLine("Enter a number of threads in your ThreadPool:");
             ThreadPoolLib.ThreadPool threadPool = new ThreadPoolLib.ThreadPool(GetNumber());
-            for (int i = 0; i < 20; i++) 
+            for (int i = 0; i < 50; i++) 
             {
                 threadPool.Enqueue(action);
                 Thread.Sleep(10);
