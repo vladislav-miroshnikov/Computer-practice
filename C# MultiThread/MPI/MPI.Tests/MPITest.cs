d53f48e0f79651d1e6260e0a76678ef830486065
vladislav-miroshnikov <vladislaw.miroshnikov@gmail.com>
2020-10-19 14:33:47 +0300

    MPI with quick sort using a regular set of samples realized

---
+++ C# MultiThread/MPI/MPI.Tests/MPITest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MPI.Tests
+{
+    [TestClass]
+    public class MPITest
+    {
+        private string expt;
+        private string actual;
+       
+        private void Prepare()
+        {
+            ArrayGenerator.GenerateArray();
+            actual = Path.Combine(Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\"))) + "actual.txt";
+            string args = $"mpiexec -n 4 MPI.exe {ArrayGenerator.FileFirst} {actual}";
+            expt = ArrayGenerator.FileSecond;
+            string filePath = Path.Combine(Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\"))) + @"MPI\bin\Debug\";
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+            cmd.StandardInput.WriteLine($"cd {filePath}");
+            cmd.StandardInput.WriteLine(args);
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+        }
+
+        [TestMethod]
+        public void MPICheck()
+        {
+            Prepare();
+            Assert.IsTrue(CompareArrays(expt, actual));
+        }
+
+        private bool CompareArrays(string fileFirst, string fileSecond)
+        {
+            if (!File.Exists(fileFirst))
+            {
+                Console.WriteLine(string.Format("File {0} does not exist.", fileFirst));
+                return false;
+            }
+
+            if (!File.Exists(fileSecond))
+            {
+                Console.WriteLine(string.Format("File {0} does not exist.", fileSecond));
+                return false;
+            }
+
+            string contentsFirst = File.ReadAllText(fileFirst);
+            string contentsSecond = File.ReadAllText(fileSecond);
+
+            if (contentsFirst.Length != contentsSecond.Length)
+            {
+                Console.WriteLine("File sizes are different");
+                return false;
+            }
+
+            for (int i = 0; i < contentsFirst.Length; i++)
+            {
+                if (contentsFirst[i] != contentsSecond[i])
+                {
+                    Console.WriteLine(string.Format("Files are different at position {0}", i));
+                    return false;
+                }
+            }
+
+            Console.WriteLine("Files are the same");
+            return true;
+        }
+    }
+}
---
+++ C# MultiThread/MPI/MPI/QSort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MPI
+{
+    internal static class QSort
+    {
+        static int Partition<T>(List<T> array, int a, int b) where T : IComparable<T>
+        {
+            int i = a;
+            for (int j = a; j <= b; j++)         
+            {
+                if (array[j].CompareTo(array[b]) <= 0)  
+                {
+                    T t = array[i];                  
+                    array[i] = array[j];                 
+                    array[j] = t;                    
+                    i++;                         
+                }
+            }
+            return i - 1;                      
+        }
+
+        public static void Quicksort<T>(List<T> array, int firstBorder, int secondBorder) where T : IComparable<T>
+        {
+            if (firstBorder >= secondBorder)
+            {
+                return;
+            }
+            int pivot = Partition(array, firstBorder, secondBorder);
+            Quicksort(array, firstBorder, pivot - 1);
+            Quicksort(array, pivot + 1, secondBorder);
+        }
+
+
+    }
+}
