d45021db76a085531e7eff3f5e8746e2abf70b6b
vladislav-miroshnikov <vladislaw.miroshnikov@gmail.com>
2020-10-19 13:50:25 +0300

    MPI task added

---
+++ semester 3/MPI/MPI.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MPI.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MPI.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e24392b4-bbd0-46fb-9f34-1fb460ea4600")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ semester 3/MPI/MPI/ArrayGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace MPI
+{
+    public static class ArrayGenerator
+    {
+        public static string FileFirst { get; private set; }
+        public static string FileSecond { get; private set; }
+
+        public static void GenerateArray()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            Random r = new Random(DateTime.Now.Millisecond);
+
+            int capacity = 1500000;
+
+            List<int> lst = new List<int>(capacity);
+            for (int i = 0; i < capacity; i++)
+            {
+                lst.Add(r.Next(100000));
+            }
+            string filePath = Path.Combine(Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\")));
+            FileFirst = filePath + "testUnsort.txt";
+            FileSecond = filePath + "testSort.txt";
+            File.WriteAllText(FileFirst, string.Join(" ", lst.Select(x => x.ToString())));
+
+            lst.Sort();
+
+            File.WriteAllText(FileSecond, string.Join(" ", lst.Select(x => x.ToString())));
+            sw.Stop();
+        }     
+    }
+}
